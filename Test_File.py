b27449c8b1eb773eceebfdc9dd5386d8c08317d0
sa.tabunkin <tsagrom@gmail.com>
2021-08-05 15:21:11 +0300

    Обновил файл Test_ 05.08.2021 15_20_55

--- Test_File.py
+++ Test_File.py
@@ -10,10 +10,3 @@ import pyperclip
 def calc(x):
     return str(math.log(abs(12 * math.sin(int(x)))))
 
-browser = webdriver.Chrome()
-
-browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
-# говорим Selenium проверять в течение 12 секунд, пока не появится текст '$100'
-button = WebDriverWait(browser, 12).until(
-        EC.text_to_be_present_in_element((By.ID, "price"),'$100'))
