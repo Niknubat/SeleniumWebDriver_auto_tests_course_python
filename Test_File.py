5e0297d0bd5e43b100c9499edb3d7e022e133aab
sa.tabunkin <tsagrom@gmail.com>
2021-08-10 23:02:14 +0300

    Опыты над тестовым файлом 10.08.2021 23_02_10

--- Test_File.py
+++ Test_File.py
@@ -1,12 +1,28 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-import time
-import math
-import pyperclip
+import pytest
 
 
-def calc(x):
-    return str(math.log(abs(12 * math.sin(int(x)))))
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
 
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        pass
+        # какие-то проверки
+
+    def test_second_smiling_faces(self, prepare_faces):
+        pass
+        # какие-то проверки
