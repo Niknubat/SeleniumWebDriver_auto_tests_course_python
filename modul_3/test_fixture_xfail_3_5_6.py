b7b7655aba72b19cdcd257f867e7874e8a74541a
sa.tabunkin <tsagrom@gmail.com>
2021-08-11 15:45:17 +0300

    Задание: пропуск тестов Параметр strict Если тест будет неожиданно пройден (XPASS), то это приведет к падению всего тестового набора.

---
+++ modul_3/test_fixture_xfail_3_5_6.py
@@ -0,0 +1,23 @@
+# Задание: пропуск тестов
+
+#Параметр strict
+# Ни XFAIL, ни XPASS по умолчанию не приводят к падению всего набора тестов.
+# Но это можно изменить, установив параметру strict значение True:
+# В этом случае, если тест будет неожиданно пройден (XPASS),
+# то это приведет к падению всего тестового набора.
+
+import pytest
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
