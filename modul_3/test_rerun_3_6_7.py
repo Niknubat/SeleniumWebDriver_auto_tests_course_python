bddaa1c5e5c0996908860ca38f5c1b5792aec142
sa.tabunkin <tsagrom@gmail.com>
2021-08-12 18:23:57 +0300

    Плагины и перезапуск тестов. плагин pytest-rerunfailures - для перезапуска упавшего теста

---
+++ modul_3/test_rerun_3_6_7.py
@@ -0,0 +1,18 @@
+# Комманда для запуска: pytest -v --tb=line --reruns 1 --browser_name=chrome test_rerun.py
+
+# Чтобы указать количество перезапусков для каждого из упавших тестов, нужно добавить в командную строку параметр:
+# "--reruns n", где n — это количество перезапусков.
+# Если при повторных запусках тесты пройдут успешно, то и прогон тестов будет считаться успешным.
+# Количество перезапусков отображается в отчёте, благодаря чему можно позже анализировать проблемные тесты.
+
+# Дополнительно мы указали параметр "--tb=line", чтобы сократить лог с результатами теста.
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
