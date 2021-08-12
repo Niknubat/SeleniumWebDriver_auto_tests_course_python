e6ac2d482228eee2e3813636b88000d3a8f1a32c
sa.tabunkin <tsagrom@gmail.com>
2021-08-12 18:02:14 +0300

    Проверка открытия браузеров из Conftest.py

---
+++ modul_3/test_parser_3_6_6.py
@@ -0,0 +1,9 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+    time.sleep(2)
