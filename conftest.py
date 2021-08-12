bed5670058e67e38270f47073653c0f17d1bba76
sa.tabunkin <tsagrom@gmail.com>
2021-08-12 18:00:54 +0300

    Conftest.py и передача параметров в командной строке. Делается с помощью встроенной функции pytest_addoption и фикстуры request

--- conftest.py
+++ conftest.py
@@ -16,3 +16,30 @@ def browser():
     yield browser
     print("\nquit browser..")
     browser.quit()
+
+
+#   Передача параметров в командной строке
+def pytest_addoption(parser):   # встроенная функция pytest_addoption
+    # В таком случае в коммандной строке обязательно указывается какой браузер запустить
+    # parser.addoption('--browser_name', action='store', default=None,
+                     # help="Choose browser: chrome or firefox")
+    # По умолчанию хром - если не вписывать параметр в ком строке
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):   # Фикстура - request. Это все относится "def pytest_addoption(parser):"
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
